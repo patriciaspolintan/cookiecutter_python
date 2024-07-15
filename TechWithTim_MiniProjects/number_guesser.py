6ac41a39e485ea81b698921e31d7ddab3696cb3a
tishsp <trishasp@duck.com>
2024-07-15 18:24:15 +0800

    project 3

---
+++ TechWithTim_MiniProjects/number_guesser.py
@@ -0,0 +1,42 @@
+# random.randrange(), random.randint(), isdigit(), quit(), continue, break, elif
+
+import random
+# how to make a random number
+# print(random.randrange(start, stop))
+# print(random.randrange(stop))
+# r = random.randint(-5, 11) will include 11
+
+top_of_range = input("Type a number:  ")
+
+if top_of_range.isdigit(): # isdigit() = to make sure that the inputted number is a number
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess:  ")
+    if user_guess.isdigit(): # isdigit() = to make sure that the inputted number is a number
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it correct!")
+        break
+    elif user_guess > random_number:
+            print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses.")
